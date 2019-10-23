e8122ee88dd824a2dfacfab866bb4896c803a404
Greg Garvey <boznik@boznik.com>
2019-10-23 13:22:01 -0700

    added type to build return for signup block

--- src/Plugin/Block/SignupBlock.php
+++ src/Plugin/Block/SignupBlock.php
@@ -22,6 +22,7 @@ class SignupBlock extends BlockBase {
   public function build() {
 
     return [
+      '#type' => 'inline_template',
       '#id' => 'signup_block',
       '#theme' => 'signup_block'
     ];
