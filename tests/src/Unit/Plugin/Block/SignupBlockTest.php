cd71a4fa41eff3ad5c2544c55328f605c8dad2f9
boznik <boznik@boznik.com>
2020-04-16 16:00:29 -0700

    Adding tests for signup block form

--- tests/src/Unit/Plugin/Block/SignupBlockTest.php
+++ tests/src/Unit/Plugin/Block/SignupBlockTest.php
@@ -26,6 +26,11 @@ class SignupBlockTest extends UnitTestCase {
     $this->blockObject = new SignupBlock([], '', ['provider' => 'stanford_news']);
   }
 
+  public function testBlockForm() {
+    $form = $this->blockObject->blockForm();
+    $input = 'https://stanford.us14.list-manage.com/subscribe/post?u=84e3c1df5a210f020250ee1d6&amp;id=74d8f03797';
+  }
+
   public function testBlockMethod() {
     $build = $this->blockObject->build();
     $this->assertArrayEquals(['#theme' => 'signup_block'], $build);
